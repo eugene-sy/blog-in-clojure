6c6a789680a1e6dba3440b43247c34d2f217bfe8
Axblade <eugene.sypachev@gmail.com>
2013-04-06 14:39:51 +0400

    CRUD in posts

--- src/blog_in_clojure/helpers/common.clj
+++ src/blog_in_clojure/helpers/common.clj
@@ -9,3 +9,6 @@
 			(take 100 input) "...")
 		input))
 
+(defn now [] 
+	(new java.util.Date))
+
--- src/blog_in_clojure/models/post.clj
+++ src/blog_in_clojure/models/post.clj
@@ -1,17 +1,22 @@
 (ns blog-in-clojure.models.post
-	(:require [monger.collection :as mc]))
+	(:require [monger.collection :as mc]
+						[blog-in-clojure.helpers.common :as ch]))
 
 (def collection "posts")
 
 (defn find-all []
 	(mc/find-maps collection))
 
-(defn find-one [])
+(defn find-one [id]
+	(mc/find-maps collection {:_id id}))
 
-(defn create [])
+(defn create [title body]
+	(mc/insert collection {:title title
+		:body body
+		:created (ch/now)}))
 
-(defn update [])
+(defn update [id title body]
+	(mc/update {:_id id} {:title title :body body}))
 
-(defn delete [])
-
-(defn total [])
+(defn delete [id]
+	(mc/remove {:_id id}))
