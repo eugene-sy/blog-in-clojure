2b18fbcb42c7782b3fd3bd394f2a01213dff90d8
Eugene Sypachev <eugene.sypachev@gmail.com>
2014-05-22 00:09:47 +0400

    deps

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject blog-in-clojure "0.1.0-SNAPSHOT"
+(defproject blog-in-clojure "0.2.0"
             :description "Simple blog, something like hello world in clojure"
-            :dependencies [[org.clojure/clojure "1.5.1"]
-                           [noir "1.3.0-beta3"]
-                           [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-                           [com.novemberain/monger "1.5.0"]]
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                                    [compojure "1.1.8"]
+                                    [hiccup "1.0.5"]
+                           [noir "1.3.0-beta3"]  ; deprecated
+                                    [org.markdownj/markdownj "0.3.0-1.0.2b4"]
+                                    [com.novemberain/monger "2.0.0-rc1"]]
+            :plugins [[lein-ring "0.8.10"]]
             :main blog-in-clojure.server)
 
